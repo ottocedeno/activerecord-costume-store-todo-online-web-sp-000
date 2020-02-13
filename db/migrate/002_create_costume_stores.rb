8e83f96beda9c607c25332a74fe67fbd536da423
Otto Cedeno <otto@ottostacos.com>
2020-02-13 03:45:30 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores <ActiveRecord::Migration[5.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      t.boolean
+      t.boolean :still_in_business
     end
   end
 end
