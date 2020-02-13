0002faa85c8d913b749d732503071fb5329f16a2
Otto Cedeno <otto@ottostacos.com>
2020-02-13 03:56:13 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,6 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :theme
       t.integer :price
       t.boolean :family_friendly
+      t.datetime :opening_date
     end
   end
 end
