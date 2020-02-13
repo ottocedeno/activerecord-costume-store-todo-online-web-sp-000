28466a1729a26c0e7d34e935697f69922e9926e9
Otto Cedeno <otto@ottostacos.com>
2020-02-13 03:39:12 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
     create_table :costumes do |t|
       t.string :name
       t.integer :price
-      t.string :imageurl
+      t.string :image_url
       t.string :size
       t.timestamps
     end
